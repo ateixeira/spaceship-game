76590346016334dd25c5680779f127ad302614d0
Andre Teixeira <andre.teixeira@jumbo.com>
2020-10-09 22:33:36 +0200

    :beers: Hello world!

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 import "./styles.scss";
+
+const div = document.createElement("div");
+const helloWorld = document.createTextNode("👋 🌎");
+div.appendChild(helloWorld);
+document.body.appendChild(div);
