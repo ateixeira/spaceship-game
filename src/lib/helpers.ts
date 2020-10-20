b4a810c7c35dd41a0bbc43c5e83aa9dedffda6b6
Andre Teixeira <andre.teixeira@jumbo.com>
2020-10-20 23:45:48 +0200

    :sparkles: Make an integer flag on the helper random function

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,6 +1,11 @@
-export const randomNumber = (min: number = 0, max: number = 1) => {
+export const randomNumber = (
+  min: number = 0,
+  max: number = 1,
+  integer: boolean = true
+) => {
   if (min > max) {
     throw new Error("Min should not be greather than max");
   }
-  return min + Math.random() * (max - min);
+  const rNum = min + Math.random() * (max - min);
+  return integer ? Math.round(rNum) : rNum;
 };
